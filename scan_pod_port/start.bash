501000207287422a2a2c494ff0d528ada9ddb5c4
TomNewChao <tom_toworld@163.com>
2023-07-07 14:12:19 +0800

    test scan-pid-port

--- scan_pod_port/start.bash
+++ scan_pod_port/start.bash
@@ -5,10 +5,9 @@ kubeconfig_path=$1
 
 echo "------------start------------"
 cp deploy.yaml deploy_demo.yaml
-sed -i ${namespace} deploy_demo.yaml
-sed -i ${kubeconfig_path} deploy_demo.yaml
+sed -i 's/{namespace}/${namespace}g' deploy_demo.yaml
 kubectl apply -f deploy_demo.yaml -n ${namespace} --kubeconfig=${kubeconfig_path}
-example_pod
+example_pod=`kubectl get pod -n ${namespace} --kubeconfig=${kubeconfig_path}|grep scan-containers-port`
 kubectl cp $kubeconfig_path ${example_pod}:/opt/scan_pod_port/kubeconfig.yaml -n ${namespace} --kubeconfig=${kubeconfig_path}
 kubectl kubectl exec -it ${example_pod} /bin/bash python3 /opt/scan_pod_port/scan_pod_port.py --namespace=${namespace} -n ${namespace} --kubeconfig=${kubeconfig_path}
 kubectl kubectl logs ${example_pod} -n ${namespace} --kubeconfig=${kubeconfig_path}
