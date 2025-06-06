e45894c6e55a2ccba957a1668e8050085a1a0639
TomNewChao <tom_toworld@163.com>
2025-06-06 09:48:02 +0800

    remove log record

--- scan_etherpad/scan_etherpad.py
+++ scan_etherpad/scan_etherpad.py
@@ -37,17 +37,14 @@ class Logger(object):
         'crit': logging.CRITICAL
     }
 
-    def __init__(self, filename, level='info', when='D', back_count=3,
+    def __init__(self, filename, level='info',
                  fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
         self.logger = logging.getLogger(filename)
         format_str = logging.Formatter(fmt)
         self.logger.setLevel(self.level_relations.get(level))
         sh = logging.StreamHandler(sys.stdout)
         sh.setFormatter(format_str)
-        th = handlers.TimedRotatingFileHandler(filename=filename, when=when, backupCount=back_count, encoding='utf-8')
-        th.setFormatter(format_str)
         self.logger.addHandler(sh)
-        self.logger.addHandler(th)
 
 
 logger = Logger('scan_etherpad.log').logger
